e8e1a97af303409f2d4267201979640cbd9d6eba
Abiria <abiria.dev@gmail.com>
2023-09-12 15:54:58 +0900

    refactor: separate window logic into `lib.rs`

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+use std::num::NonZeroU32;
+
+use softbuffer::{Context, Surface};
+use winit::{
+	dpi::PhysicalSize,
+	event_loop::EventLoop,
+	window::{Window, WindowId},
+};
+
+pub struct Color(pub [u8; 3]);
+
+impl Color {
+	fn to_u32(&self) -> u32 {
+		(self.0[0] as u32) << 2 * u8::BITS
+			| (self.0[1] as u32) << u8::BITS
+			| self.0[2] as u32
+	}
+}
+
+pub struct ColorWindow {
+	window: Window,
+	color: Color,
+	ctx: Context,
+	surface: Surface,
+}
+
+impl ColorWindow {
+	pub fn new(event_loop: &EventLoop<()>, color: Color) -> Self {
+		let window = Window::new(&event_loop).unwrap();
+
+		let ctx = unsafe { Context::new(&window) }.unwrap();
+
+		let surface = unsafe { Surface::new(&ctx, &window) }.unwrap();
+
+		Self {
+			window,
+			color,
+			ctx,
+			surface,
+		}
+	}
+
+	pub fn id(&self) -> WindowId { self.window.id() }
+
+	pub fn draw(&mut self) {
+		let PhysicalSize { width, height } = self.window.inner_size();
+		self.surface
+			.resize(
+				NonZeroU32::new(width).unwrap(),
+				NonZeroU32::new(height).unwrap(),
+			)
+			.unwrap();
+
+		let mut buf = self.surface.buffer_mut().unwrap();
+
+		let color = self.color.to_u32();
+
+		buf.fill(color);
+		buf.present().unwrap();
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,21 @@
-use std::num::NonZeroU32;
-
-use softbuffer::{Context, Surface};
+use window_prism::{Color, ColorWindow};
 use winit::{
-	dpi::PhysicalSize,
 	event::{Event, WindowEvent},
 	event_loop::EventLoop,
-	window::Window,
 };
 
 fn main() -> anyhow::Result<()> {
-	//
 	let event_loop = EventLoop::new();
 
-	let window = Window::new(&event_loop)?;
-
-	let ctx = unsafe { Context::new(&window) }.unwrap();
-
-	let mut surface = unsafe { Surface::new(&ctx, &window) }.unwrap();
+	let mut color_window =
+		ColorWindow::new(&event_loop, Color([100, 100, 100]));
 
 	event_loop.run(move |event, _, ctrl_flow| {
 		ctrl_flow.set_wait();
 
 		match event {
 			Event::WindowEvent { window_id, event }
-				if window_id == window.id() =>
+				if window_id == color_window.id() =>
 				match event {
 					// WindowEvent::Resized(_) => todo!(),
 					// WindowEvent::Moved(_) => todo!(),
@@ -32,18 +24,7 @@ fn main() -> anyhow::Result<()> {
 				},
 			// Event::MainEventsCleared => todo!(),
 			Event::RedrawRequested(_) => {
-				let PhysicalSize { width, height } = window.inner_size();
-				surface
-					.resize(
-						NonZeroU32::new(width).unwrap(),
-						NonZeroU32::new(height).unwrap(),
-					)
-					.unwrap();
-
-				let mut buf = surface.buffer_mut().unwrap();
-
-				buf.fill(0);
-				buf.present().unwrap();
+				color_window.draw();
 			},
 			Event::LoopDestroyed => (),
 			_ => (),
